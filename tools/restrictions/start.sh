f8126dcc474b6ff327cbb0c180373b8f8890b9d6
Peter Brodersen <peter@ter.dk>
2026-06-02 10:13:41 +0200

    OSRM restriction backend

--- tools/restrictions/start.sh
+++ tools/restrictions/start.sh
@@ -11,4 +11,7 @@ fi
 docker run --rm -t -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-extract -p /data/${LUAFILE:?} /data/${OSMFILE:?}
 docker run --rm -t -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-partition /data/${OSMFILE:?}
 docker run --rm -t -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-customize /data/${OSMFILE:?}
-docker run --rm -p 127.0.0.1:5000:5000 --name osrm-restrictions -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-routed --algorithm mld /data/${OSMFILE:?}
+
+docker run -d --restart unless-stopped -p 127.0.0.1:5010:5010 --name osrm-restrictions -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-routed --port 5010 --algorithm mld /data/${OSMFILE:?}
+
+docker run -d --restart unless-stopped -t -i --env-file .env -p 127.0.0.1:9976:9966 --name osrm-denmark-exclude-frontend ghcr.io/project-osrm/osrm-frontend:latest
